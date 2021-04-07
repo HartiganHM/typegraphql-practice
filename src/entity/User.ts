cba2dcf33073a287cc8740ea580775f8fc13c582
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-04-06 19:18:36 -0600

    Update User entity for GQL

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,27 @@
+import { Field, ID, ObjectType } from 'type-graphql';
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 
-@Entity()
-class User extends BaseEntity {
+@ObjectType() // Converts TS to GraphQL type
+@Entity() // DB Type for typeorm
+class User extends BaseEntity { // Extending BaseEntity allows for typeorm DB functions like .find/.create
+  @Field(() => ID) // Makes this field queryable in GQL; Add specific GQL Type as a return
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   firstName: string;
 
+  @Field()
   @Column()
   lastName: string;
 
+  @Field()
   @Column('text', { unique: true })
   email: string;
+
+  @Field() // Only part of GQL schema, not DB
+  name: string;
 
   @Column()
   password: string;
