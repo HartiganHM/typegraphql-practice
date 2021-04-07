ec8f9a12040e222ac603f1cae461fe37018b91fd
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-04-06 18:27:06 -0600

    Build initial server

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from 'apollo-server-express';
+import * as Express from 'express';
+import { buildSchema, Query, Resolver } from 'type-graphql';
+import 'reflect-metadata';
+
+const PORT = 4000;
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String, { name: 'helloWorld' }) // GQL type
+  async hello() {
+    return 'Hello World!';
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(PORT, () =>
+    console.log(`🚀  Server running on http://localhost:${PORT}/graphql`)
+  );
+};
+
+main();
